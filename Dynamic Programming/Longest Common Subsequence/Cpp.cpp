42a45642a8dcfc5ab81e2a7f5aa700d23e515b1d
koreakk <ahaseo11@gmail.com>
2022-11-16 10:18:17 +0900

    LCS

---
+++ Dynamic Programming/Longest Common Subsequence/Cpp.cpp
@@ -0,0 +1,39 @@
+#include <utility>
+#include <string>
+#include <vector>
+using namespace std;
+
+string LCS(const string& x, const string& y) {
+    const size_t xSize = x.size();
+    const size_t ySize = y.size();
+
+    vector<vector<int>> dp(xSize + 1, vector<int>(ySize + 1, 0));
+    for (size_t i = 1; i <= xSize; ++i) {
+        for (size_t j = 1; j <= ySize; ++j) {
+            if (x[i - 1] == y[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            }
+            else {
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+    }
+
+    size_t i = xSize, j = ySize;
+    string result;
+
+    while (dp[i][j]) {
+        if (max(dp[i - 1][j], dp[i][j - 1]) < dp[i][j]) {
+            result = x[i - 1] + result;
+            --i, --j;
+        }
+        else if (dp[i - 1][j] > dp[i][j - 1]) {
+            --i;
+        }
+        else {
+            --j;
+        }
+    }
+
+    return result;
+}
